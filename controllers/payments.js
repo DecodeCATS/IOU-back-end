6025f6dcd10394890862271f56b4c77065f29af6
Antonio Quintero Felizzola <antonio@Antonios-MacBook-Pro.local>
2017-08-18 15:20:09 -0400

    Function addPaymentForContract fully implemented

--- controllers/payments.js
+++ controllers/payments.js
@@ -38,7 +38,35 @@ module.exports = (dataLoader) => {
 
         dataLoader.getAllPaymentsForContract(req.user, req.params.id)
             .then(paymentsArray => {
-                console.log(paymentsArray);
+
+                var mapPaymentsArray = paymentsArray.map(payments => {
+                    var obj = {
+                        paymentId: payments.payment_id,
+                        contractId: payments.contract_id,
+                        type: payments.payment_type,
+                        amount: payments.payment_amount,
+                        status: payments.payment_status,
+                        dueDate: payments.due_date,
+                        paidDate: payments.payment_date,
+                        createdAt: payments.created_at,
+                        updatedAt: payments.updated_at,
+                    };
+                    return obj;
+                });
+
+                var paymentObj = {
+                    payments: mapPaymentsArray
+                };
+                res.status(200).json(paymentObj);
+            })
+            .catch(err => res.status(400).json({error: err.message}));
+
+    });
+
+    paymentsController.post('/contracts', onlyLoggedIn, (req, res) => {
+
+        dataLoader.addPaymentForContract(req.user, req.body)
+            .then(paymentsArray => {
 
                 var mapPaymentsArray = paymentsArray.map(payments => {
                     var obj = {
